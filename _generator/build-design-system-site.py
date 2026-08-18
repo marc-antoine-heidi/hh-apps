2e6fa2742b279c6a5d0c408d6bcd317ab036ba6e
Marc(-Antoine) <marc-antoine@heidihealth.com>
2026-08-18 10:28:12 +1000

    Mark Text page as WIP

--- _generator/build-design-system-site.py
+++ _generator/build-design-system-site.py
@@ -416,7 +416,7 @@ assert STATUS_LABEL.keys() == STATUS_MEANING.keys(), "every status needs a legen
 # Screens are the same case: a capture of a shipped screen is not on the refactor journey.
 SECTION_STATUS = {"Brand": None, "Foundations": "todo", "Components": "todo",
                   "Screens": None}
-STATUS = {"colors.html": "live", "fonts.html": "live", "icons.html": "live",
+STATUS = {"colors.html": "live", "fonts.html": "wip", "icons.html": "live",
           "sheets.html": "wip"}
 
 

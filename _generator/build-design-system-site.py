3527f3e81c9825db469ad299a91674cbd4bcb471
Marc(-Antoine) <marc-antoine@heidihealth.com>
2026-09-01 07:20:25 +1000

    Rebuild design-system site from heidinative-ios 4cde0d5bb + e3b0f0f45

--- _generator/build-design-system-site.py
+++ _generator/build-design-system-site.py
@@ -425,7 +425,7 @@ assert STATUS_LABEL.keys() == STATUS_MEANING.keys(), "every status needs a legen
 SECTION_STATUS = {"Brand": None, "Foundations": "todo", "Components": "todo",
                   "Screens": None}
 STATUS = {"colors.html": "live", "fonts.html": "wip", "spacing.html": "live",
-          "radius.html": "wip", "surfaces.html": "wip", "icons.html": "live",
+          "radius.html": "live", "surfaces.html": "wip", "icons.html": "live",
           "sheets.html": "wip"}
 
 

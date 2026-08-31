bad7db0d6a75386c328bb5c3f2abd3a87fedbb05
Marc(-Antoine) <marc-antoine@heidihealth.com>
2026-08-31 15:55:20 +1000

    Rebuild design-system site from heidinative-ios 4cde0d5bb

--- _generator/build-design-system-site.py
+++ _generator/build-design-system-site.py
@@ -424,7 +424,7 @@ assert STATUS_LABEL.keys() == STATUS_MEANING.keys(), "every status needs a legen
 SECTION_STATUS = {"Brand": None, "Foundations": "todo", "Components": "todo",
                   "Screens": None}
 STATUS = {"colors.html": "live", "fonts.html": "wip", "spacing.html": "live",
-          "radius.html": "wip", "icons.html": "live", "sheets.html": "wip"}
+          "radius.html": "live", "icons.html": "live", "sheets.html": "wip"}
 
 
 def status_of(href):
